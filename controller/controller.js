671e569d10b0f00440af8b820dbc97d692dc35c3
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-11-03 17:28:56 +0100

    update

--- controller/controller.js
+++ controller/controller.js
@@ -3,8 +3,31 @@ const express = require("express")
 const router = express()
 
 router.post("/hng-task", (req, res) => {
-    const {x, y, operation_type} = req.body
-    res.send(req.body)
+const {x, y, operation_type} = req.body;
+  const operation_types = ["addition", "subtraction", "multiplication"];
+  if(!operation_types.includes(operation_type)){
+      return res.status(400).json({ok : false, message : "Invalid operation type provided."});
+  }
+  let result;
+  switch(operation_type){
+      case "addition":
+          result = x + y;
+          break;
+      case "subtraction":
+          result = x - y;
+          break;
+      case "multiplication":
+          result = x * y;
+          break;
+      default:
+          break;
+  }
+  
+  if(!result) 
+  {
+      return res.status(400).json({ok : false, message : "Cant proceed with request input is empty."});
+  }
+  return res.status(200).json({ok : true, SlackName : "Samson onifade", operation_type, result})
 })
 
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const Port = process.env.PORT || 4000
 const controller = require("./controller/controller")
 app.set("view engine", "ejs")
-
+app.use(express.json())
 app.get("/", (req, res) => {
     res.render("index")
   })
@@ -20,30 +20,6 @@ app.get("/hng-task1", (req, res) => {
 
 app.use("/", controller)
 // app.post("/hng-task2", (req, res) => {
-//   const {x, y, operation_type} = req.body;
-//   const operation_types = ["addition", "subtraction", "multiplication"];
-//   if(!operation_types.includes(operation_type)){
-//       return res.status(400).json({ok : false, message : "Invalid operation type provided."});
-//   }
-//   let result;
-//   switch(operation_type){
-//       case "addition":
-//           result = x + y;
-//           break;
-//       case "subtraction":
-//           result = x - y;
-//           break;
-//       case "multiplication":
-//           result = x * y;
-//           break;
-//       default:
-//           break;
-//   }
-  
-//   if(!result) 
-//   {
-//       return res.status(400).json({ok : false, message : "Cant proceed with request input is empty."});
-//   }
-//   return res.status(200).json({ok : true, SlackName : "Samson onifade", operation_type, result})
+
 // })
 app.listen(Port)
